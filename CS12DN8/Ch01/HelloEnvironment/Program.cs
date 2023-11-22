808a863a2ebcc349b9f54ea695a0fe0333e60b0c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-22 23:01:28 +0530

    2nd program

--- CS12DN8/Ch01/HelloEnvironment/Program.cs
+++ CS12DN8/Ch01/HelloEnvironment/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloEnvironment;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ForegroundColor = ConsoleColor.DarkCyan;
+
+        WriteLine(Env.CurrentDirectory);
+        WriteLine(Env.OSVersion.VersionString);
+        WriteLine("Namespace: {0}!", typeof(Program).Namespace);
+
+        ResetColor();
+
+        WriteLine("Press any key ...");
+        ReadKey();
+    }
+}
