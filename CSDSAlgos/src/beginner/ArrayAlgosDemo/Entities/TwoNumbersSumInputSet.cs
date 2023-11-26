119dfad4878108f95d9b4c17a6358b779141e0b5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-26 13:40:17 +0530

    Code Refactoring

--- CSDSAlgos/src/beginner/ArrayAlgosDemo/Entities/TwoNumbersSumInputSet.cs
+++ CSDSAlgos/src/beginner/ArrayAlgosDemo/Entities/TwoNumbersSumInputSet.cs
@@ -6,12 +6,12 @@ internal class TwoNumbersSumInputSet
 
     public int SumToMatch { get; set; }
 
-    public static List<TwoNumbersSumInputSet> GenerateTwoNumbersSumInputs() => new()
-            {
-                new TwoNumbersSumInputSet { InputNumbers = new int[] { 3, 5, -4, 8, 11, 1, -1, 6 }, SumToMatch = 10 },
-                new TwoNumbersSumInputSet { InputNumbers = new int[] { 2, 4, 6, 8, 10 }, SumToMatch = 12 },
-                new TwoNumbersSumInputSet { InputNumbers = new int[] { -3, 1, 5, 8, -2 }, SumToMatch = 6 },
-                new TwoNumbersSumInputSet { InputNumbers = new int[] { 0, 1, 2, 3, 4 }, SumToMatch = 8 },
+    public static List<TwoNumbersSumInputSet> GenerateTwoNumbersSumInputs() =>
+            [
+                new TwoNumbersSumInputSet { InputNumbers = [3, 5, -4, 8, 11, 1, -1, 6], SumToMatch = 10 },
+                new TwoNumbersSumInputSet { InputNumbers = [2, 4, 6, 8, 10], SumToMatch = 12 },
+                new TwoNumbersSumInputSet { InputNumbers = [-3, 1, 5, 8, -2], SumToMatch = 6 },
+                new TwoNumbersSumInputSet { InputNumbers = [0, 1, 2, 3, 4], SumToMatch = 8 },
                 new TwoNumbersSumInputSet { InputNumbers = null, SumToMatch = 15 }, // Test case with null InputNumbers
-            };
+            ];
 }
--- CSDSAlgos/src/common/HeaderFooter/Header.cs
+++ CSDSAlgos/src/common/HeaderFooter/Header.cs
@@ -7,7 +7,7 @@ public class Header : IHeader
 
     public void DisplayHeader(char header, string title, int length = 100, ConsoleColor foregroundColor = ConsoleColor.Yellow)
     {
-        var leftPadValue = ((length - title.Length) / 2) + title.Length;
+        int leftPadValue = ((length - title.Length) / 2) + title.Length;
         string headerValue = new(header, length);
 
         ForegroundColor = foregroundColor;
