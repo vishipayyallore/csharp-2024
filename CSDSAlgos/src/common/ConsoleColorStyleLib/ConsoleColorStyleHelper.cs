7103580d459673322caf17aba78ecc0c486cfd3f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-11-26 13:27:17 +0530

    Initial Version

---
+++ CSDSAlgos/src/common/ConsoleColorStyleLib/ConsoleColorStyleHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleColorStyleLib;
+
+public static class ConsoleColorStyleHelper
+{
+    public static ConsoleColor GetConsoleForegroundColor(ConsoleColorStyle style) => style switch
+    {
+        ConsoleColorStyle.DarkYellow => ConsoleColor.DarkYellow,
+
+        ConsoleColorStyle.DarkCyan => ConsoleColor.DarkCyan,
+
+        ConsoleColorStyle.DarkGreen => ConsoleColor.DarkGreen,
+
+        ConsoleColorStyle.DarkMagenta => ConsoleColor.DarkMagenta,
+
+        ConsoleColorStyle.Red => ConsoleColor.Red,
+
+        _ => ConsoleColor.White
+    };
+
+}
