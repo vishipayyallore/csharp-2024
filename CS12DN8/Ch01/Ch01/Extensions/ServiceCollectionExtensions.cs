da2dfaa6617cb829202167d2f319d240c641eb46
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-05 19:54:08 +0530

    ServiceCollectionExtensions

---
+++ CS12DN8/Ch01/Ch01/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using HeaderFooter;
+using HeaderFooter.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ch01.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+
+    public static IServiceCollection ConfigureServices(this IServiceCollection services)
+    {
+        _ = services.AddTransient<IFooter, Footer>();
+        _ = services.AddTransient<IHeader, Header>();
+
+        return services;
+    }
+
+}
