3e79d378262d58819192d1fa2af05310674b9449
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-05 20:10:10 +0530

    Update Program.cs

--- CS12DN8/Ch01/Ch01/Program.cs
+++ CS12DN8/Ch01/Ch01/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Ch01.Extensions;
+using Ch01.Interfaces;
+
+using IHost? host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.ConfigureServices();
+    })
+    .Build();
+
+host.Services.GetRequiredService<IRunnable>()?.Run();
+
+WriteLine("\n\nPress any key ... ");
+ReadKey();
