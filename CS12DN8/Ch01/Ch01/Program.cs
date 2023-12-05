a4c6af37729a6af56bfb7c1658127bf6387eadd8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-05 20:11:30 +0530

    Code Refactoring

--- CS12DN8/Ch01/Ch01/Program.cs
+++ CS12DN8/Ch01/Ch01/Program.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Ch01.Extensions;
+﻿using Ch01.Extensions;
 using Ch01.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 using IHost? host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((_, services) =>
