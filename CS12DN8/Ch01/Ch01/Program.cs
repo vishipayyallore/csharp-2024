e724076a7e4ea8fff65c0acd3cedeb6821e411d7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-07 15:14:42 +0530

    Code Refactoring

--- CS12DN8/Ch01/Ch01/Program.cs
+++ CS12DN8/Ch01/Ch01/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Ch01.Extensions;
 using Ch01.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Reflection;
 
 using IHost? host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((_, services) =>
@@ -10,38 +10,16 @@ using IHost? host = Host.CreateDefaultBuilder(args)
     })
     .Build();
 
+// Get IRunnableManager from the service provider
+IRunnableManager? runnableManager = host.Services.GetRequiredService<IRunnableManager>();
+
 // Discover and instantiate IRunnable instances in the Runnables namespace
-IEnumerable<IRunnable?>? runnableInstances = GetRunnableInstances("Ch01.Runnables");
+IEnumerable<IRunnable?>? runnableInstances = runnableManager.GetRunnableInstances("Ch01.Runnables");
 
 foreach (IRunnable? runnable in runnableInstances!)
 {
-    RunRunnable(runnable!);
+    runnableManager.RunRunnable(runnable!);
 }
 
 WriteLine("\n\nPress any key ... ");
 ReadKey();
-
-//static IEnumerable<IRunnable?>? GetRunnableInstances(string namespacePrefix)
-//{
-//    Type? runnableInterfaceType = typeof(IRunnable);
-//    Assembly? assembly = Assembly.GetExecutingAssembly();
-
-//    List<IRunnable?>? runnableInstances = assembly.GetTypes()
-//        .Where(type => type.IsClass && type.Namespace == namespacePrefix && runnableInterfaceType.IsAssignableFrom(type))
-//        .Select(type => (IRunnable?)Activator.CreateInstance(type))
-//        .ToList();
-
-//    return runnableInstances;
-//}
-
-//static void RunRunnable(IRunnable runnable)
-//{
-//    try
-//    {
-//        runnable.Run();
-//    }
-//    catch (Exception ex)
-//    {
-//        WriteLine($"An error occurred while running {runnable.GetType().Name}: {ex.Message}");
-//    }
-//}
--- CS12DN8/Ch01/Ch01/Utilities/RunnableManager.cs
+++ CS12DN8/Ch01/Ch01/Utilities/RunnableManager.cs
@@ -1,20 +1,33 @@
 ﻿using Ch01.Interfaces;
+using Microsoft.Extensions.Logging;
 using System.Reflection;
 
 namespace Ch01.Utilities;
 
-public class RunnableManager : IRunnableManager
+public class RunnableManager(ILogger<RunnableManager> logger) : IRunnableManager
 {
+    private readonly ILogger<RunnableManager> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
     public IEnumerable<IRunnable?> GetRunnableInstances(string namespacePrefix)
     {
         Type runnableInterfaceType = typeof(IRunnable);
         Assembly assembly = Assembly.GetExecutingAssembly();
 
+        if (runnableInterfaceType == null || assembly == null)
+        {
+            _logger.LogError("Unable to retrieve types due to null reflection objects.");
+            return Enumerable.Empty<IRunnable?>();
+        }
+
         List<IRunnable?> runnableInstances = assembly.GetTypes()
             .Where(type => type.IsClass && type.Namespace == namespacePrefix && runnableInterfaceType.IsAssignableFrom(type))
             .Select(type => (IRunnable?)Activator.CreateInstance(type))
             .ToList();
+
+        if (!runnableInstances.Any())
+        {
+            _logger.LogWarning($"No runnables found in namespace '{namespacePrefix}'.");
+        }
 
         return runnableInstances;
     }
@@ -27,7 +40,7 @@ public class RunnableManager : IRunnableManager
         }
         catch (Exception ex)
         {
-            WriteLine($"An error occurred while running {runnable.GetType().Name}: {ex.Message}");
+            _logger.LogError($"An error occurred while running {runnable.GetType().Name}: {ex.Message}");
         }
     }
 }
