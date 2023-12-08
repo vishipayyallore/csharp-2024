c9adde14813dce743f40da250e3d5d4be303f0e2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-08 10:28:49 +0530

    Update Program.cs

--- CS12DN8/Ch01/Ch01/Program.cs
+++ CS12DN8/Ch01/Ch01/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Ch01.Extensions;
 using Ch01.Interfaces;
+using HeaderFooter.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,7 @@ using IHost? host = Host.CreateDefaultBuilder(args)
 
 // Get IRunnableManager from the service provider
 IRunnableManager? runnableManager = host.Services.GetRequiredService<IRunnableManager>();
+IFooter? _footer = host.Services.GetRequiredService<IFooter>();
 
 // Discover and instantiate IRunnable instances in the Runnables namespace
 IEnumerable<IRunnable?>? runnableInstances = runnableManager.GetRunnableInstances("Ch01.Runnables");
@@ -19,6 +21,8 @@ IEnumerable<IRunnable?>? runnableInstances = runnableManager.GetRunnableInstance
 foreach (IRunnable? runnable in runnableInstances!)
 {
     runnableManager.RunRunnable(runnable!);
+
+    _footer.DisplayFooter('*', 140, ConsoleColor.DarkMagenta);
 }
 
 WriteLine("\n\nPress any key ... ");
