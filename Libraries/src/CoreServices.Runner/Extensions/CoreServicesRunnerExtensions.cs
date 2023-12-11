df684921797ac425fdb999ce0c7cf89c5ea3485c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-11 13:25:14 +0530

    Swamy/10dec work (#19)
    * Initial Version
    
    * Code Refactoring
    
    * Code Refactoring
    
    * Changes
    
    * Added ReadMe files
    
    * Added the Tag
    
    * Code Refactoring
    
    * Update CoreServices.Runner.csproj
    
    * Removed the files which were moved into Library
    
    * Create ServiceCollectionExtensions.cs
    
    * Added the Readme.md files
    
    * Update CoreServices.Runner.csproj

---
+++ Libraries/src/CoreServices.Runner/Extensions/CoreServicesRunnerExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CoreServices.Runner.Interfaces;
+using HeaderFooter;
+using HeaderFooter.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoreServices.Runner.Extensions;
+
+public static class CoreServicesRunnerExtensions
+{
+
+    public static IServiceCollection ConfigureRunnerServices(this IServiceCollection services)
+    {
+        _ = services.AddTransient<IFooter, Footer>();
+        _ = services.AddTransient<IHeader, Header>();
+
+        _ = services.AddTransient<IRunnableManager, RunnableManager>();
+
+        return services;
+    }
+
+}
