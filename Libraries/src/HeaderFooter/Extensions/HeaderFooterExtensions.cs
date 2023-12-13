d4b9116fb3ceb2735e0911343b92b84fe5ed472a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-13 22:12:59 +0530

    Code Refactoring

--- Libraries/src/HeaderFooter/Extensions/HeaderFooterExtensions.cs
+++ Libraries/src/HeaderFooter/Extensions/HeaderFooterExtensions.cs
@@ -6,7 +6,7 @@ namespace HeaderFooter.Extensions;
 public static class HeaderFooterExtensions
 {
 
-    public static IServiceCollection ConfigureRunnerServices(this IServiceCollection services)
+    public static IServiceCollection ConfigureHeaderFooterServices(this IServiceCollection services)
     {
         _ = services.AddTransient<IFooter, Footer>();
         _ = services.AddTransient<IHeader, Header>();
