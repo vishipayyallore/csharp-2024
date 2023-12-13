cec9870365786b16141e501c8aeb1fcdebef8d3c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-13 22:28:13 +0530

    Changes

---
+++ CS12DN8/Ch02/CompilerVersion/Program.cs
@@ -0,0 +1,15 @@
+﻿ForegroundColor = ConsoleColor.DarkCyan;
+
+WriteLine("Uncomment the lines to See A. Compiler Version, and B. Auto Generate <Main>$().");
+WriteLine("//  #error version");
+WriteLine("//  throw new Exception();");
+
+// Compiler version: '4.9.0-1.23560.3 (78d23490)'.Language version: 12.0.Vocabulary  
+//  #error version
+
+ResetColor();
+
+// Uncomment this line to throw an exception and show Auto Generate Method name
+// Program.<Main>$(String[] args)
+//  throw new Exception();
+
---
+++ CS12DN8/Ch02/Vocabulary/Program.cs
@@ -0,0 +1,10 @@
+﻿ForegroundColor = ConsoleColor.DarkCyan;
+
+WriteLine("Hello, C# 12, and DotNet 8 !");
+
+string name = typeof(Program).Namespace ?? "None!";
+WriteLine($"Namespace: {name}");
+
+ResetColor();
+
+
