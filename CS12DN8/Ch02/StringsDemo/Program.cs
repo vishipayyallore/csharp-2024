36319b606b644f14e8595d372308effb0332948b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-01-08 13:57:36 +0530

    Update Program.cs

--- CS12DN8/Ch02/StringsDemo/Program.cs
+++ CS12DN8/Ch02/StringsDemo/Program.cs
@@ -9,15 +9,15 @@ string phoneNumber = "(215) 555-4256";
 WriteLine($"Name: {firstName} {lastName}, Phone: {phoneNumber}");
 
 OutputEncoding = System.Text.Encoding.UTF8;
-WriteLine($"Emoji: {char.ConvertFromUtf32(0x1F600)}");
+WriteLine($"\nEmoji: {char.ConvertFromUtf32(0x1F600)}");
 
 // Assigning a string with escape sequences.
 string fullNameWithTabSeparator = "Bob\tSmith";
-WriteLine($"Full Name with Separator: {fullNameWithTabSeparator}");
+WriteLine($"\nFull Name with Separator: {fullNameWithTabSeparator}");
 
 //Verbatim strings
 string filePath = @"C:\televisions\sony\bravia.txt";
-WriteLine($"(Verbatim strings) File Path: {filePath}");
+WriteLine($"\n(Verbatim strings) File Path: {filePath}");
 
 // Raw strings literals
 string xml = """
@@ -25,11 +25,23 @@ string xml = """
             <first_name>Mark</first_name>
             </person>
             """;
-WriteLine($"(Raw strings) XML: \n{xml}");
+WriteLine($"\n(Raw strings) XML: \n{xml}");
 
 xml = """
         <person age="50">
         <first_name>Mark</first_name>
         </person>
 """;
-WriteLine($"(Raw strings) XML with Indentation: \n{xml}");
+WriteLine($"\n(Raw strings) XML with Indentation: \n{xml}");
+
+var person = new { FirstName = "Alice", Age = 56 };
+
+// Raw interpolated string literals
+string json = $$"""
+                {
+                "first_name": "{{person.FirstName}}",
+                "age": {{person.Age}},
+                "calculation": "{{{1 + 2}}}"
+                }
+                """;
+WriteLine($"\n(Raw interpolated strings) JSON: \n{json}");
