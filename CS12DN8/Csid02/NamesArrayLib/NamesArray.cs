65ce261ed921183b2149ebe469d4cdebfc8937cf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-09 12:37:54 +0530

    Changes

---
+++ CS12DN8/Csid02/NamesArrayLib/NamesArray.cs
@@ -0,0 +1,15 @@
+﻿using NamesArrayLib.Interfaces;
+using NamesGenerator.Interfaces;
+
+namespace NamesArrayLib;
+
+public class NamesArray(IGenerateNames nameGenerator) : INamesArray
+{
+    private readonly IGenerateNames _nameGenerator = nameGenerator ?? throw new ArgumentNullException(nameof(nameGenerator));
+
+    public string[] GenerateNames(int nameLength = 10, int numberOfNames = 10)
+    {
+        return _nameGenerator.GetNames(nameLength, numberOfNames);
+    }
+
+}
