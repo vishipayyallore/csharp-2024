d760cc16a0ddaa709a1c9c7900c5091434e5ff45
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-02-09 12:47:05 +0530

    Code Refactoring

--- CS12DN8/Csid02/Csid02/Program.cs
+++ CS12DN8/Csid02/Csid02/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HeaderFooter;
+using HeaderFooter.Interfaces;
+using NamesArrayLib;
+using NamesArrayLib.Interfaces;
+using NamesGenerator;
+using PrintHelperLib;
+using PrintHelperLib.Interfaces;
+
+Run();
+
+static void Run()
+{
+    INamesArray _namesArray = new NamesArray(new GenerateNames());
+    IPrintHelper _printHelper = new PrintHelper();
+    IHeader _header = new Header();
+    IFooter _footer = new Footer();
+
+    Random _random = new();
+
+    _header.DisplayHeader('=', "Names Array Demo");
+
+    ForegroundColor = ConsoleColor.Cyan;
+    _printHelper.Print(_namesArray.GenerateNames(nameLength: _random.Next(5, 10), numberOfNames: 10));
+    ResetColor();
+
+    _footer.DisplayFooter('-');
+}
+
