82a7e5411941494541fe3b283fa90bfd6897c249
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-16 11:46:29 +0530

    Update Program.cs

--- CS12DN8/Ch02/CompilerVersion/Program.cs
+++ CS12DN8/Ch02/CompilerVersion/Program.cs
@@ -5,10 +5,10 @@ WriteLine("1. //  #error version");
 WriteLine("2. //  throw new Exception();");
 
 // Compiler version: '4.11.0-3.24321.9 (847d5884)'. Language version: 12.0.
-#error version
+// #error version
 
 ResetColor();
 
 // Uncomment this line to throw an exception and show Auto Generate Method name """Program.<Main>$(String[] args)"""
-throw new Exception();
+// throw new Exception();
 
