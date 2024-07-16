b098bb9d790cc100bdb56be9b19ac0d2052234ba
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-16 11:24:45 +0530

    Changes

--- CS12DN8/Ch01/HelloCS/Program.cs
+++ CS12DN8/Ch01/HelloCS/Program.cs
@@ -13,7 +13,6 @@ foreach (string arg in args)
 
 WriteLine("CurrentDirectory: {0}", Env.CurrentDirectory);
 WriteLine("OSVersion.VersionString: {0}", Env.OSVersion.VersionString);
-WriteLine("Namespace: {0}!", typeof(Program).Namespace);
 
 ResetColor();
 
