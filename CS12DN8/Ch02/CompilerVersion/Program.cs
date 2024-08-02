4240ec96eaac866b2280558af83d1921f8fdbf94
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-02 17:43:07 +0530

    Update Program.cs

--- CS12DN8/Ch02/CompilerVersion/Program.cs
+++ CS12DN8/Ch02/CompilerVersion/Program.cs
@@ -1,7 +1,7 @@
 ﻿ForegroundColor = ConsoleColor.DarkCyan;
 
-WriteLine("Uncomment the lines to See A. Compiler Version, and B. Auto Generate <Main>$().");
-WriteLine("1. //  #error version");
+WriteLine("Uncomment the lines to See \n\tA. Compiler Version. \n\tB. Auto Generate <Main>$() method.");
+WriteLine("\n1. //  #error version");
 WriteLine("2. //  throw new Exception();");
 
 // Compiler version: '4.11.0-3.24321.9 (847d5884)'. Language version: 12.0.
