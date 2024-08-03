0a5eb662f69ab7a6027715a94b1147a079169d39
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-03 14:51:56 +0530

    New csproj for ch02

---
+++ CSBigPicture/src/ch02/Program.cs
@@ -0,0 +1,19 @@
+﻿
+ShowApproachable(args);
+
+static void ShowApproachable(string[] args)
+{
+    WriteLine("Showing Approachable Demo");
+
+    if (args.Length == 0)
+    {
+        WriteLine("No arguments were provided.");
+        return;
+    }
+
+    for (int n = 0; n < args.Length; n++)
+    {
+        WriteLine("arg[{0}] = {1}", n, args[n]);
+    }
+
+}
