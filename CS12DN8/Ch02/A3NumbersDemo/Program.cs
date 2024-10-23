da64f8b07cd853b2dd64597404a714b5d4a09302
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-10-23 14:40:03 +0530

    Update Program.cs

--- CS12DN8/Ch02/A3NumbersDemo/Program.cs
+++ CS12DN8/Ch02/A3NumbersDemo/Program.cs
@@ -31,23 +31,24 @@ static void ShowNumberVariables()
 
 static void ExploringWholeNumbers()
 {
+    WriteLine("\nWhole Numbers:");
     int decimalNotation = 2_000_000;
     int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
     int hexadecimalNotation = 0x_001E_8480;
 
     // Check the three variables have the same value.
-    Console.WriteLine($"{decimalNotation == binaryNotation}");
-    Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+    WriteLine($"{decimalNotation == binaryNotation}");
+    WriteLine($"{decimalNotation == hexadecimalNotation}");
 
     // Output the variable values in decimal.
-    Console.WriteLine($"{decimalNotation:N0}");
-    Console.WriteLine($"{binaryNotation:N0}");
-    Console.WriteLine($"{hexadecimalNotation:N0}");
+    WriteLine($"{decimalNotation:N0}");
+    WriteLine($"{binaryNotation:N0}");
+    WriteLine($"{hexadecimalNotation:N0}");
 
     // Output the variable values in hexadecimal.
-    Console.WriteLine($"{decimalNotation:X}");
-    Console.WriteLine($"{binaryNotation:X}");
-    Console.WriteLine($"{hexadecimalNotation:X}");
+    WriteLine($"{decimalNotation:X}");
+    WriteLine($"{binaryNotation:X}");
+    WriteLine($"{hexadecimalNotation:X}");
 }
 
 static void ShowSizeOfTypes()
